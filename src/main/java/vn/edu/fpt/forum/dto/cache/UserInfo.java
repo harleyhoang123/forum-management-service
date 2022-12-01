ac5588d349b099c43d634756d25770fff791bc3e
harleyhoang123 <harley.hoang.work@gmail.com>
2022-12-02 01:32:44 +0700

    Change account info data

--- src/main/java/vn/edu/fpt/forum/dto/cache/UserInfo.java
+++ src/main/java/vn/edu/fpt/forum/dto/cache/UserInfo.java
@@ -20,12 +20,13 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 @Builder
-@JsonPropertyOrder(value = {"username", "email", "fullName", "roles"})
+@JsonPropertyOrder(value = {"username", "email", "fullName", "avatar", "roles"})
 public class UserInfo implements Serializable {
 
     private static final long serialVersionUID = 4689073615796931874L;
     private String username;
     private String email;
     private String fullName;
+    private String avatar;
     private List<String> roles;
 }
